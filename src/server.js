bbf0cb42474c6578e1028698b0ace63c70aaeb88
Yeremia Yoga <yogayeremia@gmail.com>
2026-04-29 10:42:15 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -6,5 +6,12 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
+  const lastUpdate = new Date().toLocaleString("id-ID", {
+    timeZone: "Asia/Jakarta",
+    dateStyle: "full", 
+    timeStyle: "medium" 
+  });
+
   console.log(`🟢 Silane Server (Foundry Gateway) is running on port ${PORT}`);
+  console.log(`📅 Last Start/Update: ${lastUpdate} WIB`);
 });
