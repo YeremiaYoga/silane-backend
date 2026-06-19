5ffb5865e34936333c1dc82b9f9d0d840f007bd6
Yeremia Yoga <yogayeremia@gmail.com>
2026-06-19 17:19:21 +0700

    update

--- src/models/igniteCharacterModel.js
+++ src/models/igniteCharacterModel.js
@@ -3,7 +3,7 @@ import supabase from "../utils/db.js";
 export const getIgniteCharactersByUserId = async (userId) => {
   return await supabase
     .from("characters")
-    .select("id, name, full_name, art_image, token_image, created_at, species, classes")
+    .select("id, name, full_name, art_image, token_image, created_at, species, classes, fvtt_format")
     .eq("user_id", userId)
     .eq("record_status", "active")
     .order("created_at", { ascending: false });
